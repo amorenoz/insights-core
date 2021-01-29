51527339eb1023dcf7d7c6fdd525f50492363258
Adrian Moreno <amorenoz@redhat.com>
2021-01-29 13:18:00 +0100

    parsers: improve ofcl_dump_flows parser
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- insights/parsers/ovs_ofctl_dump_flows.py
+++ insights/parsers/ovs_ofctl_dump_flows.py
@@ -39,6 +39,21 @@ class OVSofctlDumpFlows(CommandParser):
             2
     """
 
+    def __init__(self, *args, **kwargs):
+        self._field_decoders = {
+            "duration": lambda x: float(x.replace('s', '')),
+            "table": int,
+            "idle_age": int,
+            "importance": int,
+            "hard_age": int,
+            "priority": int,
+            "in_port": self._decode_port,
+            "n_bytes": int,
+            "n_packets": int,
+        }
+
+        super(OVSofctlDumpFlows, self).__init__(*args, **kwargs)
+
     def parse_content(self, content):
         if not content:
             raise SkipException("Empty Content!")
@@ -52,12 +67,32 @@ class OVSofctlDumpFlows(CommandParser):
             raise SkipException("Invalid Path!")
 
         for line in content:
-            line = line.split(',')
-            flow_list = split_kv_pairs(line, split_on='=')
+            if self._is_header(line):
+                continue
+            # Actions are a whole different thing that might contain multiple ','s and '='s
+            # They should be handled separately. In fact, I don't know if blind sting splitting
+            # would work. Imagine this:
+            # actions=strip_vlan,load:0x5->NXM_NX_REG13[],load:0x4->NXM_NX_REG11[],load:0x9->NXM_NX_REG12[],load:0xc->OXM_OF_METADATA[],load:0x4->NXM_NX_REG14[],resubmit(,8)
+            line_parts = line.split("actions=")
+
+            if len(line_parts) != 2:
+                raise SkipException("Invalid Content!")
+
+            flow_list = split_kv_pairs(line_parts[0].split(","), split_on='=')
             if flow_list:
-                self._bridges.append(flow_list)
+                flow = dict(map(self._decode_field, flow_list.items()))
+                flow['actions'] = line_parts[1]
+                self._bridges.append(flow)
+
         if not self._bridges:
             raise SkipException("Invalid Content!")
+
+    def _decode_field(self, elem):
+        return (elem[0], elem[1]) if elem[0] not in self._field_decoders else (
+            elem[0], self._field_decoders[elem[0]](elem[1]))
+
+    def _is_header(self, line):
+        return 'NXST_FLOW' in line
 
     @property
     def bridge_name(self):
@@ -74,3 +109,14 @@ class OVSofctlDumpFlows(CommandParser):
         empty list `[]` on failure.
         """
         return self._bridges
+
+    def _decode_port(self, port):
+        """
+        ports can be either numerical or strings
+        (int) or (str) depending on input field
+        """
+        try:
+            return int(port)
+        except ValueError:
+            ## Treat it as a string. Remove the extra quotes
+            return port.replace('"', '')
